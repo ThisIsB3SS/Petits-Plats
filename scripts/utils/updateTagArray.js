de0b89c4843746841a7b95ea8aa5e4a73aea2596
Sebastien_Sq <sebastien.escudero@gmail.com>
2024-09-16 23:05:25 +0200

    refactor: deleted all console.log

--- scripts/utils/updateTagArray.js
+++ scripts/utils/updateTagArray.js
@@ -8,23 +8,17 @@ let filteredRecipes = [];
 
 export function addTagToArray(tag) {
   tagArray.push(tag);
-  console.log("tag added = ", tag);
-  console.log("tagArray = ", tagArray);
+
   // Update the search
  filteredRecipes = mainSearch(allRecipes, mainInput.value, tagArray);
  showRecipes(filteredRecipes);
 }
 
 export function removeTagFromArray(tag) {
-    console.log("Initial tagArray = ", tagArray);
     const index = tagArray.indexOf(tag);
     if (index !== -1) {
       let deletedTag = tagArray.splice(index, 1);
-      console.log("deletedTag = ", deletedTag);
-    } else {
-      console.log("Tag not found: ", tag);
-    }
-    console.log("Updated tagArray = ", tagArray);
+    } 
     
     // Update the search
     filteredRecipes = mainSearch(allRecipes, mainInput.value, tagArray);
